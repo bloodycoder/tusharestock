fa7f6cfc074dba6b9b738baca15639f786bb09ec
Picard Xie <picardxie@foxmail.com>
2023-08-27 13:50:36 +0800

    updat
    update

---
+++ calculator.py
@@ -0,0 +1,6 @@
+def cal(jinzichan, roe):
+    #5 years 
+    for i in range(5,11):
+        gujia = jinzichan *(roe**i)
+        print("year ",i,":",gujia)
+    #10 years
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 import load
 from mail import *
 # 设置tushare的token，需要先在tushare官网注册并获取
-load.initconfig()
+#load.initconfig()
 ftoken = open("token", "r")
 token = ftoken.readline().strip()
 ts.set_token(token)
@@ -16,7 +16,7 @@ flog =  open('log.txt', 'a')
 pro = ts.pro_api()
 
 # 打开JSON文件并读取内容
-with open('config.json', 'r') as f:
+with open('config.json', 'r' ,encoding='utf-8') as f:
     json_data = json.load(f)
 
 # 遍历股票代码及其买入点和卖出点
@@ -29,6 +29,10 @@ for stock_code, points in json_data.items():
     print("股票代码：", stock_code)
     name = str(points["name"])
     df = pro.daily(ts_code=stock_code, start_date=date_str, end_date=date_str)
+    if(df.shape[0] == 0):
+        closeprice = 1000000
+        json_data[stock_code]["closeprice"] = closeprice
+        continue
     closeprice = float(df.iloc[0][5])
     #closeprice = random.randint(0,100)
     json_data[stock_code]["closeprice"] = closeprice
@@ -49,8 +53,11 @@ sorted_data = sorted(json_data.items(), key=sort_function)
 print(sorted_data)
 for stock_code, points in sorted_data:
     name = str(points["name"])
-    buypoint = float(points["buypoint"])
-    sellpoint = float(points["sellpoint"])
+    jingzichan = float(points["jingzichan"])
+    roe_low = float(points["roe_low"])
+    roe_high = float(points["roe_high"])
+    buypoint = float(jingzichan * ((1+(roe_low/100))**5))
+    sellpoint = float(jingzichan * ((1+(roe_high/100))**5))
     holdstate = str(points["hold_state"])
     closeprice = float(points["closeprice"])
     guxirate = 100*(float(points["guxi"]/float(points["closeprice"])))
@@ -60,7 +67,7 @@ for stock_code, points in sorted_data:
     if(closeprice > sellpoint):
         log += stock_code + " should be sell\n"
     rate = (closeprice-buypoint)/(sellpoint-buypoint)
-    log += stock_code + " rate:" + '{:.3f}'.format(rate) + " buypoint:" + str(buypoint) + " sellpoint: " + str(sellpoint) + "\n"
+    log += stock_code + " rate:" + '{:.3f}'.format(rate) + " buypoint:" + '{:.3f}'.format(buypoint) + " sellpoint: " + '{:.3f}'.format(sellpoint) + "\n"
     log += "guxi rate:" + '{:.3f}'.format(guxirate) + '\n'
     log += "-------------------------\n"
 flog.write(log)
