19bbbe5d7c63d7a4694d6e035446c1cf1432cd1f
Picard Xie <picardxie@foxmail.com>
2023-08-29 22:39:34 +0800

    213
     123123

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-def cal(jinzichan, roe):
+def cal(jinzichan,rate, roe):
     #5 years 
     for i in range(5,11):
-        gujia = jinzichan *(roe**i)
+        gujia = jinzichan *(roe**i) - jinzichan*(1-rate)
         print("year ",i,":",gujia)
     #10 years
