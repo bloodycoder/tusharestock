472359b09d362040ae68018b5955536d9353659e
Picard Xie <picardxie@foxmail.com>
2023-08-30 23:04:31 +0800

    updatA

--- cal_roe.py
+++ cal_roe.py
@@ -5,12 +5,11 @@ import datetime
 import load
 from mail import *
 # 设置tushare的token，需要先在tushare官网注册并获取
-load.initconfig()
 ftoken = open("token", "r")
 token = ftoken.readline().strip()
 ts.set_token(token)
 ftoken.close()
-flog =  open('log.txt', 'a')
+flog =  open('log.txt', 'a',encoding='utf-8')
 
 # 初始化tushare的pro接口
 pro = ts.pro_api()
@@ -57,26 +56,22 @@ for stock_code, points in sorted_data:
     roe_low = float(points["roe_low"])
     roe_high = float(points["roe_high"])
     youzhi_rate = float(points["youzhi_rate"])
-    buypoint = float(points["buypoint"])
-    sellpoint = float(points["sellpoint"])
     holdstate = str(points["hold_state"])
     closeprice = float(points["closeprice"])
     guxirate = 100*(float(points["guxi"]/float(points["closeprice"])))
     log += name + "\nhold state:" + holdstate + '\n'
-    if(closeprice < buypoint):
-        log += stock_code + " should be buy\n"
-    if(closeprice > sellpoint):
-        log += stock_code + " should be sell\n"
-    rate = (closeprice-buypoint)/(sellpoint-buypoint)
-    log += stock_code + " rate:" + '{:.3f}'.format(rate) + " buypoint:" + '{:.3f}'.format(buypoint) + " sellpoint: " + '{:.3f}'.format(sellpoint) + "\n"
-    log += "guxi rate:" + '{:.3f}'.format(guxirate) + '\n'
+    for i in range(5, 10):
+        log += "year " + str(i) + " roe:\n"
+        buypoint = float(jingzichan * ((1+(roe_low/100))**i)) - jingzichan*(1-youzhi_rate)
+        sellpoint = float(jingzichan * ((1+(roe_high/100))**i)) - jingzichan*(1-youzhi_rate)
+        log += " buypoint:" + '{:.3f}'.format(buypoint) + " sellpoint: " + '{:.3f}'.format(sellpoint) + "\n"
     log += "-------------------------\n"
 flog.write(log)
 robot = PicardSendMail()
 robot.Login()
 title = "today stock info:" + date_str
-robot.SendAnEmail(email=log,subject=title,destination="510297127@qq.com")
-robot.SendAnEmail(email=log,subject=title,destination="2409029740@qq.com")
+#robot.SendAnEmail(email=log,subject=title,destination="510297127@qq.com")
+#robot.SendAnEmail(email=log,subject=title,destination="2409029740@qq.com")
 robot.Quit()
 # 调用get_realtime_quotes()函数获取实时行情数据
 flog.close()
